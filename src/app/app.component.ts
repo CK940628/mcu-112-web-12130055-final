2d42e55067c8eb2372f31c46913109fbec5b53e3
CK940628 <a0909977931@gmail.com>
2024-01-05 14:44:22 +0800

    建立nav-bar把header的內容放到nav-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
