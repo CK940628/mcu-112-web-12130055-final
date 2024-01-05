81dfdc1db748c566314ea8bbd9aa7b3ef23e9da9
CK940628 <a0909977931@gmail.com>
2024-01-05 16:41:56 +0800

    feat(nav)新增4個路由切換按鈕

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavBarComponent],
+  imports: [HeaderComponent, NavBarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
