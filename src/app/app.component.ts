5b71b46e9fd6cc2ebbc4ade1fbc4e0f543be4cfe
CK940628 <a0909977931@gmail.com>
2024-01-05 17:13:25 +0800

    feat(home)顯示首頁標題

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavBarComponent, RouterOutlet],
+  imports: [NavBarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
