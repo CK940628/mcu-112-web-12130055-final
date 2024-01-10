e8914f5ef1161917f682769a13c7ce0371ea90d4
CK940628 <a0909977931@gmail.com>
2024-01-10 10:09:59 +0800

    feat(tag-list): 標籤清單元件實作

--- src/app/tag-list/tag-list.component.ts
+++ src/app/tag-list/tag-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './tag-list.component.html',
-  styleUrl: './tag-list.component.css'
+  styleUrl: './tag-list.component.css',
 })
 export class TagListComponent {
-
+  @HostBinding('class') class = 'tag-list';
 }
