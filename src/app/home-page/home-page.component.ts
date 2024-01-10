df1ebeee1e2b1cfa4d0ccd311a99dab2b98cf588
CK940628 <a0909977931@gmail.com>
2024-01-10 08:57:56 +0800

    revision(body): 把container的內容移到body，並把container檔案刪掉

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
-import { ContainerComponent } from '../container/container.component';
+import { BodyComponent } from '../body/body.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
-  imports: [CommonModule, HeaderComponent, ContainerComponent],
+  imports: [CommonModule, HeaderComponent, BodyComponent],
 })
 export class HomePageComponent {}
