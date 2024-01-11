03647ac4b6b760b12bbeca6f0446981fd9108910
CK940628 <a0909977931@gmail.com>
2024-01-11 16:40:30 +0800

    refactor(tag-sidebar): 標籤側欄元件實作新增

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,11 +1,11 @@
 import { ArticleListComponent } from './../article-list/article-list.component';
 import { Component } from '@angular/core';
-import { TagListComponent } from '../tag-list/tag-list.component';
+import { TagSidebarComponent } from '../tag-sidebar/tag-sidebar.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [ArticleListComponent, TagListComponent],
+  imports: [ArticleListComponent, TagSidebarComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css',
 })
